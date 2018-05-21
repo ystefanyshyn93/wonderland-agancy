5227345727fa9ef27cce759b6b899fa67058bfe3
Yuliya Stefanyshyn <julia.sokil.1993@gmail.com>
2018-05-21 19:27:51 +0000

    update send-form

--- js/send-form.js
+++ js/send-form.js
@@ -5,19 +5,14 @@ function sendForm(e) {
     const form = document.querySelector('.sample-form');
     if (form.checkValidity()) {
     e.preventDefault();
-    fetch('server/form', {
+    fetch('form', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
-            planning: document.querySelector('.sample-form input[name=type-of-planning]').value,
             name: document.querySelector('.sample-form input[name=name]').value,
             email: document.querySelector('.sample-form input[name=email]').value,
-            Phone: document.querySelector('.sample-form input[name=number]').value,
-            date: document.querySelector('.sample-form input[name=date]').value,
-            guests: document.querySelector('.sample-form input[name=number-of-guests]').value,
-            service: document.querySelector('.sample-form input[name=what-services-are-you-interested-in]').value,
-            budget: document.querySelector('.sample-form input[name=budget]').value,
-            
+            subject: document.querySelector('.sample-form input[name=subject]').value,
+            message: document.querySelector('.sample-form input[name=message]').value,
         })
     });
     }
